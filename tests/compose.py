f60be77a1cef335720eaf11352c7be0bb781cfcd
Alex <digwnews@gmail.com>
2015-11-21 00:14:54 +0300

    first test

--- tests/compose.py
+++ tests/compose.py
@@ -1,7 +1,24 @@
 # coding=UTF-8
 
 from page import Page, Component
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class ComposePage(Page):
     PATH = '/compose/'
+
+    def letter_params(self):
+        return LetterParams(self.driver)
+
+class LetterParams(Component):
+    RECIEVER_ADDRESS = "//textarea[@data-original-name='To']"
+    SPAN_LEGAL_EMAIL = "//span[@class='js-compose-label compose__labels__label']"
+
+    def set_reciever_address(self, address):
+        self.driver.find_element_by_xpath(self.RECIEVER_ADDRESS).send_keys(address + " ")
+
+    def is_span_right_email(self, address):
+        return self.driver.find_element_by_xpath(self.SPAN_LEGAL_EMAIL).text
+        # return WebDriverWait(self.driver, 30, 0.1).until(
+        #     lambda d: d.find_element_by_xpath(self.SPAN_LEGAL_EMAIL).text
+        # )
--- tests/write_letter.py
+++ tests/write_letter.py
@@ -1,5 +1,6 @@
 import unittest
 from login import LoginPage
+from compose import ComposePage
 
 from selenium import webdriver
 
@@ -11,10 +12,6 @@ class WriteLetterTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def tearDown(self):
-        self.driver.quit()
-
-    def test(self):
         login_page = LoginPage(self.driver)
         login_page.open()
 
@@ -22,3 +19,14 @@ class WriteLetterTest(unittest.TestCase):
         login_form.set_email_name(self.EMAIL)
         login_form.set_password(self.PASSWORD)
         login_form.submit()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_reciever_field(self):
+        compose_page = ComposePage(self.driver)
+        compose_page.open()
+        letter_params = compose_page.letter_params()
+        TEST_LEGAL_EMAIL = "test@mail.ru"
+        letter_params.set_reciever_address(TEST_LEGAL_EMAIL)
+        text = letter_params.is_span_right_email(TEST_LEGAL_EMAIL)
