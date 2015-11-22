48a978b5cfe48b1d825bcd23a7343b236b939ad3
Andrey Pavlov <dir94@mail.ru>
2015-11-22 13:16:21 +0300

    minor fixes

--- tests/write_letter_test2.py
+++ tests/write_letter_test2.py
@@ -32,19 +32,6 @@ class WriteLetterTest2(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    # def toggle_header(self, what):
-    #     header = self.header_switcher.get_row(what)
-    #     dropdown_elem = self.header_switcher.get_dropdown_list_element(what)
-    #     is_visible = header.is_displayed()
-    #
-    #     self.header_switcher_dropdown_btn.click()
-    #     dropdown_elem.click()
-    #     self.assertEqual(header.is_displayed(), not is_visible)
-    #
-    #     self.header_switcher_dropdown_btn.click()
-    #     dropdown_elem.click()
-    #     self.assertEqual(header.is_displayed(), is_visible)
-
     ##############################################
 
     def test_copy_header_toggle(self):
@@ -61,7 +48,6 @@ class WriteLetterTest2(unittest.TestCase):
         self.assertEqual(header.is_displayed(), is_visible)
 
     def test_hidden_copy_header_toggle(self):
-        # self.toggle_header(HeaderSwitcher.HIDDEN_COPY)
         header = self.header_switcher.get_row(self.header_switcher.HIDDEN_COPY)
         dropdown_elem = self.header_switcher.get_dropdown_list_element(self.header_switcher.HIDDEN_COPY)
         is_visible = header.is_displayed()
@@ -75,7 +61,6 @@ class WriteLetterTest2(unittest.TestCase):
         self.assertEqual(header.is_displayed(), is_visible)
 
     def test_from_header_toggle(self):
-        # self.toggle_header(HeaderSwitcher.FROM)
         header = self.header_switcher.get_row(self.header_switcher.FROM)
         dropdown_elem = self.header_switcher.get_dropdown_list_element(self.header_switcher.FROM)
         is_visible = header.is_displayed()
@@ -89,7 +74,6 @@ class WriteLetterTest2(unittest.TestCase):
         self.assertEqual(header.is_displayed(), is_visible)
 
     def test_priority_header_toggle(self):
-        # self.toggle_header(HeaderSwitcher.PRIORITY)
         header = self.header_switcher.get_row(self.header_switcher.PRIORITY)
         dropdown_elem = self.header_switcher.get_dropdown_list_element(self.header_switcher.PRIORITY)
         is_visible = header.is_displayed()
@@ -103,7 +87,6 @@ class WriteLetterTest2(unittest.TestCase):
         self.assertEqual(header.is_displayed(), is_visible)
 
     def test_notify_read_header_toggle(self):
-        # self.toggle_header(HeaderSwitcher.NOTIFY_READ)
         header = self.header_switcher.get_row(self.header_switcher.NOTIFY_READ)
         dropdown_elem = self.header_switcher.get_dropdown_list_element(self.header_switcher.NOTIFY_READ)
         is_visible = header.is_displayed()
@@ -117,7 +100,6 @@ class WriteLetterTest2(unittest.TestCase):
         self.assertEqual(header.is_displayed(), is_visible)
 
     def test_no_reply_header_toggle(self):
-        # self.toggle_header(HeaderSwitcher.NO_REPLY)
         header = self.header_switcher.get_row(self.header_switcher.NO_REPLY)
         dropdown_elem = self.header_switcher.get_dropdown_list_element(self.header_switcher.NO_REPLY)
         is_visible = header.is_displayed()
@@ -141,39 +123,3 @@ class WriteLetterTest2(unittest.TestCase):
         header_switcher.get_no_reply_dropdown_btn().click()
         header_switcher.get_no_reply_dropdown_list_elem(3600).click()
         self.assertEqual(header_switcher.get_no_reply_dropdown_btn_text(), u'1 час')
-
-    # def test_hidden_copy_toggle(self):
-    #     header_switcher = self.compose_page.header_switcher()
-    #     is_visible = header_switcher.get_row(header_switcher.HIDDEN_COPY).is_displayed()
-    #
-    #     header_switcher.get_dropdown_button().click()
-    #     header_switcher.get_dropdown_list_element(header_switcher.HIDDEN_COPY).click()
-    #     self.assertEqual(header_switcher.get_row(header_switcher.HIDDEN_COPY).is_displayed(), not is_visible)
-    #
-    #     header_switcher.get_dropdown_button().click()
-    #     header_switcher.get_dropdown_list_element(header_switcher.HIDDEN_COPY).click()
-    #     self.assertEqual(header_switcher.get_row(header_switcher.HIDDEN_COPY).is_displayed(), is_visible)
-    #
-    # def test_from_toggle(self):
-    #     header_switcher = self.compose_page.header_switcher()
-    #     is_visible = header_switcher.get_row(header_switcher.FROM).is_displayed()
-    #
-    #     header_switcher.get_dropdown_button().click()
-    #     header_switcher.get_dropdown_list_element(header_switcher.FROM).click()
-    #     self.assertEqual(header_switcher.get_row(header_switcher.FROM).is_displayed(), not is_visible)
-    #
-    #     header_switcher.get_dropdown_button().click()
-    #     header_switcher.get_dropdown_list_element(header_switcher.FROM).click()
-    #     self.assertEqual(header_switcher.get_row(header_switcher.FROM).is_displayed(), is_visible)
-    #
-    # def test_priority_toggle(self):
-    #     header_switcher = self.compose_page.header_switcher()
-    #     is_visible = header_switcher.get_row(header_switcher.FROM).is_displayed()
-    #
-    #     header_switcher.get_dropdown_button().click()
-    #     header_switcher.get_dropdown_list_element(header_switcher.FROM).click()
-    #     self.assertEqual(header_switcher.get_row(header_switcher.FROM).is_displayed(), not is_visible)
-    #
-    #     header_switcher.get_dropdown_button().click()
-    #     header_switcher.get_dropdown_list_element(header_switcher.FROM).click()
-    #     self.assertEqual(header_switcher.get_row(header_switcher.FROM).is_displayed(), is_visible)
